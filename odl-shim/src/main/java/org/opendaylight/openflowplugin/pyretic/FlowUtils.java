a460253483377cb82ef9522d32ceb470610a7e25
Jennifer Hernández <jhdez.becares@gmail.com>
2014-12-03 15:25:57 +0100

    Included NetIDE copyright

--- odl-shim/src/main/java/org/opendaylight/openflowplugin/pyretic/FlowUtils.java
+++ odl-shim/src/main/java/org/opendaylight/openflowplugin/pyretic/FlowUtils.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2014, NetIDE Consortium (Create-Net (CN), Telefonica Investigacion Y Desarrollo SA (TID), Fujitsu 
+ * Technology Solutions GmbH (FTS), Thales Communications & Security SAS (THALES), Fundacion Imdea Networks (IMDEA),
+ * Universitaet Paderborn (UPB), Intel Research & Innovation Ireland Ltd (IRIIL) )
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Authors:
+ *     Telefonica I+D
+ */
 package org.opendaylight.openflowplugin.pyretic;
 
 import com.google.common.collect.ImmutableList;
