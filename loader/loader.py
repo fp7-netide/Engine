b94a108216707a618021e0ca83cdb6632c2baf95
Gregor Best <gbe@mail.upb.de>
2015-04-27 11:17:20 +0200

    Add skeleton loader

---
+++ loader/loader.py
@@ -0,0 +1,22 @@
+"""
+ Copyright (c) 2014, NetIDE Consortium (Create-Net (CN), Telefonica Investigacion Y Desarrollo SA (TID), Fujitsu 
+ Technology Solutions GmbH (FTS), Thales Communications & Security SAS (THALES), Fundacion Imdea Networks (IMDEA),
+ Universitaet Paderborn (UPB), Intel Research & Innovation Ireland Ltd (IRIIL), Fraunhofer-Institut für 
+ Produktionstechnologie (IPT), Telcaria Ideas SL (TELCA) )
+
+ All rights reserved. This program and the accompanying materials
+ are made available under the terms of the Eclipse Public License v1.0
+ which accompanies this distribution, and is available at
+ http://www.eclipse.org/legal/epl-v10.html
+
+ Authors:
+     Gregor Best, gbe@mail.upb.de
+"""
+
+# TODO:
+# - Read metadata
+# - Determine App->Controller mappings
+# - Start a client controller for each app
+
+if __name__ == "__main__":
+    print("Here be dragons")
