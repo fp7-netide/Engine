60b81c41d7b01f0b3812ecaaa7591a970925659e
Gregor Best <gbe@mail.upb.de>
2015-06-05 12:17:43 +0200

    Add tests for languages, tests for those tests
    Signed-off-by: Gregor Best <gbe@mail.upb.de>

--- loader/environment.py
+++ loader/environment.py
@@ -1,53 +1,74 @@
 # Stuff that checks the environemnt: hardware, languages (?), network (?)
 
-import os
+import os, subprocess
 
 class HardwareCheckException(Exception): pass
 
 def check_hardware(h):
-	"Raises an exception if any of the requirements in req are not met by the current hardware. Raises a KeyError if an unknown \
-	check is requested"
+    "Raises an exception if any of the requirements in req are not met by the current hardware. Raises a KeyError if an unknown \
+            check is requested"
 
-	# Check CPU
-	if "cpuarch" in h:
-		m = os.uname().machine
-		a = h["cpuarch"]
-		if a == "x86" and m == "x86_64":
-			a = "x86_64"
-		if m != a:
-			raise HardwareCheckException("Invalid CPU architecture: want {}, have {}".format(a, m))
+    # Check CPU
+    if "cpuarch" in h:
+        m = os.uname().machine
+        a = h["cpuarch"]
+        if a == "x86" and m == "x86_64":
+            a = "x86_64"
+        if m != a:
+            raise HardwareCheckException("Invalid CPU architecture: want {}, have {}".format(a, m))
 
-	if "cpufreq" in h:
-		with open("/proc/cpuinfo") as f:
-			for l in f.readlines():
-				if not l.startswith("cpu MHz"):
-					continue
-				have = float(l.split(":", 2)[1])
-				if have < h["cpufreq"]:
-					raise HardwareCheckException("Invalid CPU frequency: want {} MHz, have {} MHz".format(h["cpufreq"], have))
+    if "cpufreq" in h:
+        with open("/proc/cpuinfo") as f:
+            for l in f.readlines():
+                if not l.startswith("cpu MHz"):
+                    continue
+                have = float(l.split(":", 2)[1])
+                if have < h["cpufreq"]:
+                    raise HardwareCheckException("Invalid CPU frequency: want {} MHz, have {} MHz".format(h["cpufreq"], have))
 
-	if "cpucores" in h:
-		numcpu = 0
-		with open("/proc/cpuinfo") as f:
-			for l in f.readlines():
-				if not l.startswith("processor"):
-					continue
-				numcpu += 1
-		if numcpu < h["cpucores"]:
-			raise HardwareCheckException("Invalid number of CPUs: want {}, have {}".format(h["cpucores"], numcpu))
+    if "cpucores" in h:
+        numcpu = 0
+        with open("/proc/cpuinfo") as f:
+            for l in f.readlines():
+                if not l.startswith("processor"):
+                    continue
+                numcpu += 1
+        if numcpu < h["cpucores"]:
+            raise HardwareCheckException("Invalid number of CPUs: want {}, have {}".format(h["cpucores"], numcpu))
 
-	# Check Memory
-	if "memory" in h:
-		print("checking for memory")
-		with open("/proc/meminfo") as f:
-			for l in f.readlines():
-				if not l.startswith("MemTotal:"):
-					continue
-				l = l.split(":", 1)[1].strip()
-				have = int(l.split(" ", 1)[0]) // 1024
-				if have < h["memory"]:
-					raise HardwareCheckException("Invalid amount of memory: want {} MB, have {} MB".format(h["memory"], have))
+    # Check Memory
+    if "memory" in h:
+        print("checking for memory")
+        with open("/proc/meminfo") as f:
+            for l in f.readlines():
+                if not l.startswith("MemTotal:"):
+                    continue
+                l = l.split(":", 1)[1].strip()
+                have = int(l.split(" ", 1)[0]) // 1024
+                if have < h["memory"]:
+                    raise HardwareCheckException("Invalid amount of memory: want {} MB, have {} MB".format(h["memory"], have))
 
-	for k, v in h.items():
-		if k not in ["cpuarch", "cpufreq", "cpucores", "memory"]:
-			raise KeyError(k)
+    for k, v in h.items():
+        if k not in ["cpuarch", "cpufreq", "cpucores", "memory"]:
+            raise KeyError(k)
+
+class LanguageCheckException(Exception): pass
+
+def check_languages(langs):
+    for l in langs:
+        if l.get("name") not in ["python", "java"]:
+            raise HardwareCheckException("I don't know how to check for '{}'".format(l.get("name")))
+        if l["name"] == "python":
+            if l.get("version", "").startswith("3"):
+                pbin = "python3"
+                pass
+            elif l.get("version", "").startswith("2"):
+                pbin = "python2"
+            else:
+                raise HardwareCheckException("Don't know how to check for python version '{}'".format(l.get("version")))
+            v = subprocess.check_output([pbin, "--version"]).decode('utf-8').split(" ", 1)[1].strip()
+            if not v.startswith(l.get("version", "")):
+                raise HardwareCheckException("Can't find a matching {} version. Wanted {}, got {}.".format(pbin, l["version"], v))
+            print(v)
+        print(l)
+    raise HardwareCheckException("Why am I here?")
--- loader/tests/test_environment.py
+++ loader/tests/test_environment.py
@@ -1,50 +1,55 @@
 import environment as env
 
 def test_check_hardware_arch():
-	env.check_hardware({ "cpuarch": "x86_64" })
-	env.check_hardware({ "cpuarch": "x86" })
+    env.check_hardware({ "cpuarch": "x86_64" })
+    env.check_hardware({ "cpuarch": "x86" })
 
-	try:
-		env.check_hardware({ "cpuarch": "FloopenShmitz" })
-	except env.HardwareCheckException:
-		pass
-	else:
-		assert False, "Test should have failed but didn't"
+    try:
+        env.check_hardware({ "cpuarch": "FloopenShmitz" })
+    except env.HardwareCheckException:
+        pass
+    else:
+        assert False, "Test should have failed but didn't"
 
 def test_check_hardware_cpufreq():
-	env.check_hardware({ "cpufreq": 500 })
+    env.check_hardware({ "cpufreq": 500 })
 
-	try:
-		env.check_hardware({ "cpufreq": float("inf") })
-	except env.HardwareCheckException:
-		pass
-	else:
-		assert False, "Test should have failed but didn't"
+    try:
+        env.check_hardware({ "cpufreq": float("inf") })
+    except env.HardwareCheckException:
+        pass
+    else:
+        assert False, "Test should have failed but didn't"
 
 def test_check_hardware_cpucores():
-	env.check_hardware({ "cpucores": 1 })
+    env.check_hardware({ "cpucores": 1 })
 
-	try:
-		env.check_hardware({ "cpucores": float("inf") })
-	except env.HardwareCheckException:
-		pass
-	else:
-		assert False, "Test should have failed but didn't"
+    try:
+        env.check_hardware({ "cpucores": float("inf") })
+    except env.HardwareCheckException:
+        pass
+    else:
+        assert False, "Test should have failed but didn't"
 
 def test_check_hardware_mem():
-	env.check_hardware({ "memory": 128 })
+    env.check_hardware({ "memory": 128 })
 
-	try:
-		env.check_hardware({ "memory": float("inf") })
-	except env.HardwareCheckException:
-		pass
-	else:
-		assert False, "Test should have failed but didn't"
+    try:
+        env.check_hardware({ "memory": float("inf") })
+    except env.HardwareCheckException:
+        pass
+    else:
+        assert False, "Test should have failed but didn't"
 
 def test_check_hardware_unknown_key():
-	try:
-		env.check_hardware({ "fnord": "yes" })
-	except KeyError:
-		pass
-	else:
-		assert False, "Test should have failed but didn't"
+    try:
+        env.check_hardware({ "fnord": "yes" })
+    except KeyError:
+        pass
+    else:
+        assert False, "Test should have failed but didn't"
+
+def test_check_languages():
+    env.check_languages([{"name": "python", "version": "3.4"}])
+    env.check_languages([{"name": "java", "version": "1.7"}])
+    env.check_languages([{"name": "sbcl", "version": "7.2"}])
