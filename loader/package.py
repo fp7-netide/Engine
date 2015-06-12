eaae3867e7f348667b1bbe2d0de314194e933600
Gregor Best <gbe@mail.upb.de>
2015-06-12 13:21:46 +0200

    Load packages from zip files

--- loader/package.py
+++ loader/package.py
@@ -1,6 +1,8 @@
 import inspect
 import json
 import os
+import shutil
+import tempfile
 import zipfile
 
 import controllers
@@ -10,21 +12,35 @@ from application import Application
 class Package(object):
     requirements = {}
     controllers = {}
+    cleanup = False
 
     def __init__(self, prefix, dataroot):
         self.path = os.path.abspath(prefix)
-        p = os.path.join(prefix, "_system_requirements.json")
+        if prefix.endswith(".zip") and os.path.isfile(self.path):
+            p = tempfile.mkdtemp(prefix="netide-tmp")
+            with zipfile.ZipFile(self.path) as zf:
+                zf.printdir()
+                zf.extractall(path=p)
+            self.path = p
+            self.cleanup = True
+
+        p = os.path.join(self.path, "_system_requirements.json")
         if os.path.exists(p):
             with open(p) as f:
                 self.requirements = json.load(f)
 
-        p = os.path.join(prefix, "_apps")
+        p = os.path.join(self.path, "_apps")
         for d in os.listdir(p):
             app = os.path.join(p, d)
             ctrl = Application.get_controller(app)
             if ctrl not in self.controllers:
                 self.controllers[ctrl] = ctrl(dataroot)
             self.controllers[ctrl].applications.append(Application(app))
+
+    def __del__(self):
+        if self.cleanup:
+            shutil.rmtree(self.path, ignore_errors=True)
+
 
     def __str__(self):
         return 'Package("{}")'.format(self.path)
