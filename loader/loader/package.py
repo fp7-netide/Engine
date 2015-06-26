e342b6f05e914f57c2a2d88e774aa84840cea305
Gregor Best <gbe@mail.upb.de>
2015-06-26 12:44:11 +0200

    loader: sprinkle a bit of license dust

--- loader/loader/package.py
+++ loader/loader/package.py
@@ -1,3 +1,18 @@
+"""
+ Copyright (c) 2015, NetIDE Consortium (Create-Net (CN), Telefonica Investigacion Y Desarrollo SA (TID), Fujitsu
+ Technology Solutions GmbH (FTS), Thales Communications & Security SAS (THALES), Fundacion Imdea Networks (IMDEA),
+ Universitaet Paderborn (UPB), Intel Research & Innovation Ireland Ltd (IRIIL), Fraunhofer-Institut für
+ Produktionstechnologie (IPT), Telcaria Ideas SL (TELCA) )
+
+ All rights reserved. This program and the accompanying materials
+ are made available under the terms of the Eclipse Public License v1.0
+ which accompanies this distribution, and is available at
+ http://www.eclipse.org/legal/epl-v10.html
+
+ Authors:
+     Gregor Best, gbe@mail.upb.de
+"""
+
 import inspect
 import json
 import logging
