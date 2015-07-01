9b071b198e46c2dcf5bb1b82ac86cb76fa100dba
Tim Vinkemeier <timvinkemeier@live.de>
2015-07-01 17:05:33 +0200

    Removed Netty dependency.

--- core/core.shimconnectivity/src/main/java/eu/netide/core/shimconnectivity/SocketBasedShimConnector.java
+++ core/core.shimconnectivity/src/main/java/eu/netide/core/shimconnectivity/SocketBasedShimConnector.java
@@ -1,57 +1,59 @@
 package eu.netide.core.shimconnectivity;
 
 import eu.netide.core.api.IShimConnector;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by timvi on 01.07.2015.
  */
-public class SocketBasedShimConnector implements IShimConnector {
+public class SocketBasedShimConnector implements IShimConnector, Runnable {
 
-    private ChannelFuture _channel;
-    private EventLoopGroup _bossGroup;
-    private EventLoopGroup _workerGroup;
+    private ServerSocket _socket;
+    private Socket _client;
+    private Thread _thread;
 
     public void Open(int port) {
-        _bossGroup = new NioEventLoopGroup();
-        _workerGroup = new NioEventLoopGroup();
         try {
             System.out.println("Starting shim socket server...");
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(_bossGroup, _workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new ShimSocketHandler());
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-            _channel = b.bind(port).sync();
-            System.out.println("SUCCESS! Waiting for shim connection on port " + port);
-        } catch (InterruptedException ex) {
-            System.out.println("Exception in 'Open': " + ex.getMessage());
+            _socket = new ServerSocket(port);
+            _thread = new Thread(this);
+            _thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void Close() {
         try {
             System.out.println("Closing shim connection server...");
-            _channel.channel().closeFuture().sync();
-            _workerGroup.shutdownGracefully();
-            _bossGroup.shutdownGracefully();
-        } catch (InterruptedException ex) {
-            System.out.println("Error on close: " + ex.getMessage());
-            ex.printStackTrace();
+            if (_thread != null)
+                _thread.interrupt();
+            if (_client != null)
+                _client.close();
+            _socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+
+            System.out.println("Waiting for shim connection on port " + _socket.getLocalPort());
+            _client = _socket.accept();
+            System.out.println("Client connected (" + _client.getInetAddress().getHostAddress() + "), starting handler thread.");
+            BufferedReader in = new BufferedReader(new InputStreamReader(_client.getInputStream()));
+
+            while (in.ready()) {
+                System.out.println("Shim connector received: " + in.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
