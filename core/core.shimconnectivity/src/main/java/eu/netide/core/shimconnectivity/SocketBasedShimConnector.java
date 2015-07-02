9399dc234a8a6dc01dc8609a26936c07af33ed33
Tim Vinkemeier <tim@vinkemeier.de>
2015-07-02 12:55:27 +0200

    Fixed basic shim socket server. Fixed module error in ryu-shim. Added small test for shim socket server.

--- core/core.shimconnectivity/src/main/java/eu/netide/core/shimconnectivity/SocketBasedShimConnector.java
+++ core/core.shimconnectivity/src/main/java/eu/netide/core/shimconnectivity/SocketBasedShimConnector.java
@@ -10,22 +10,20 @@ import java.net.Socket;
 
 /**
  * Created by timvi on 01.07.2015.
+ * Socket based shim connector.
  */
 public class SocketBasedShimConnector implements IShimConnector, Runnable {
 
+    private int _port;
     private ServerSocket _socket;
     private Socket _client;
     private Thread _thread;
 
     public void Open(int port) {
-        try {
-            System.out.println("Starting shim socket server...");
-            _socket = new ServerSocket(port);
-            _thread = new Thread(this);
-            _thread.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Starting shim socket server...");
+        _port = port;
+        _thread = new Thread(this);
+        _thread.start();
     }
 
     public void Close() {
@@ -33,9 +31,10 @@ public class SocketBasedShimConnector implements IShimConnector, Runnable {
             System.out.println("Closing shim connection server...");
             if (_thread != null)
                 _thread.interrupt();
-            if (_client != null)
+            if (_client != null && !_client.isClosed())
                 _client.close();
-            _socket.close();
+            if (_socket != null && !_socket.isClosed())
+                _socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,15 +42,27 @@ public class SocketBasedShimConnector implements IShimConnector, Runnable {
 
     public void run() {
         try {
-
+            _socket = new ServerSocket(_port);
             System.out.println("Waiting for shim connection on port " + _socket.getLocalPort());
             _client = _socket.accept();
-            System.out.println("Client connected (" + _client.getInetAddress().getHostAddress() + "), starting handler thread.");
+            System.out.println("Shim connected (" + _client.getInetAddress().getHostAddress() + ").");
             BufferedReader in = new BufferedReader(new InputStreamReader(_client.getInputStream()));
 
-            while (in.ready()) {
-                System.out.println("Shim connector received: " + in.readLine());
+            int byteCount;
+            byte[] buffer = new byte[5 * 1024]; // a read buffer of 5KiB
+            byte[] data;
+            String dataText;
+            while ((byteCount = _client.getInputStream().read(buffer)) > -1) {
+                data = new byte[byteCount];
+                System.arraycopy(buffer, 0, data, 0, byteCount);
+                dataText = new String(data, "UTF-8"); // assumption that client sends data UTF-8 encoded
+                System.out.println("Message from shim: " + dataText);
+                // TODO handle data
             }
+            _socket.close();
+            System.out.println("Shim server thread ended, spawning new...");
+            _thread = new Thread(this);
+            _thread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
