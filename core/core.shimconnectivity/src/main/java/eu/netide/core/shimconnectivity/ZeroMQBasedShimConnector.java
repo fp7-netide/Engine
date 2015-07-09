04f67964f962a45f9330c66199817d9fe8ba2959
Tim Vinkemeier <timvinkemeier@live.de>
2015-07-09 16:47:41 +0200

    Moved to ROUTER-DEALER messaging

--- core/core.shimconnectivity/src/main/java/eu/netide/core/shimconnectivity/ZeroMQBasedShimConnector.java
+++ core/core.shimconnectivity/src/main/java/eu/netide/core/shimconnectivity/ZeroMQBasedShimConnector.java
@@ -3,6 +3,7 @@ package eu.netide.core.shimconnectivity;
 import eu.netide.core.api.IShimConnector;
 import eu.netide.core.api.IShimMessageListener;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
 /**
  * Created by timvi on 09.07.2015.
@@ -37,19 +38,28 @@ public class ZeroMQBasedShimConnector implements IShimConnector, Runnable {
 
     public void SendMessage(String message) {
         System.out.println("Message for sending to shim: " + message);
-        _socket.send(message);
+        ZMsg msg = new ZMsg();
+        msg.add("PS1"); // TODO replace with shim id
+        msg.add("");
+        msg.add(message);
+        msg.send(_socket);
     }
 
     public void run() {
         _context = ZMQ.context(1);
         _socket = _context.socket(ZMQ.ROUTER);
-        System.out.println("Waiting for shim connection on port " + _port);
+        System.out.println("Listening for shim on port " + _port);
         _socket.bind("tcp://*:5555");
 
         while (!Thread.currentThread().isInterrupted()) {
-            byte[] message = _socket.recv();
-            System.out.println("Received " + new String(message));
-            _socket.send("ack");
+            ZMsg message = ZMsg.recvMsg(_socket);
+            String senderId = message.getFirst().toString();
+            String messageText = message.getLast().toString();
+            System.out.println("Received from '" + senderId + "':" + messageText);
+            ZMsg response = new ZMsg();
+            response.add(senderId);
+            response.add("Pingback. You sent '" + messageText + "'.");
+            response.send(_socket);
         }
         _socket.close();
     }
