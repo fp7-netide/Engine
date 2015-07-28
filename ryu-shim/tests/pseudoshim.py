ce5b77d84a0964940229e7190a1917c295a25d3d
Tim Vinkemeier <tim@vinkemeier.de>
2015-07-28 09:34:22 +0100

    Progress

--- ryu-shim/tests/pseudoshim.py
+++ ryu-shim/tests/pseudoshim.py
@@ -4,23 +4,27 @@ import threading
 import sys
 
 class CoreConnection(threading.Thread):
-    def __init__(self, id):
+    def __init__(self, port, id):
     	threading.Thread.__init__(self)
+        self.port = port
         self.id = id
 
     def run(self):
-        print('Connecting to Core...')
+        print("Connecting to Core at port " + self.port + " as '" + self.id + "'")
         context = zmq.Context()
         self.socket = context.socket(zmq.DEALER)
         self.socket.setsockopt(zmq.IDENTITY, self.id)
-        self.socket.connect("tcp://localhost:5555")
+        self.socket.connect("tcp://localhost:" + self.port)
         print('Connected.')
-        self.socket.send(b"First Hello from " + self.id)
+        #self.socket.send(b"First Hello from " + self.id)
         while True:
             message = self.socket.recv_multipart()
             print("Received message: %s" % message[0])
 
-conn = CoreConnection(sys.argv[1])
+if len(sys.argv) < 3:
+	print('You have to specify the port and then the id for the pseudoshim as arguments!')
+	sys.exit()
+conn = CoreConnection(sys.argv[1], sys.argv[2])
 conn.Daemon = True
 conn.start()
 try:
@@ -29,4 +33,4 @@ try:
         conn.socket.send(msg)
 except KeyboardInterrupt:
     conn._Thread__stop()
-    exit
+    sys.exit()
