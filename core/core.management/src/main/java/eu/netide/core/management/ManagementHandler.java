1041c675d94f6069abd27d326dae855822784ad3
Tim Vinkemeier <timvinkemeier@live.de>
2015-08-06 18:39:27 +0200

    Updated dependencies for OSGi use
    Also, the management handler now starts up and sets the port value.

--- core/core.management/src/main/java/eu/netide/core/management/ManagementHandler.java
+++ core/core.management/src/main/java/eu/netide/core/management/ManagementHandler.java
@@ -7,6 +7,8 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by timvi on 06.08.2015.
@@ -16,9 +18,12 @@ public class ManagementHandler {
     public void Start() throws IOException, MBeanException, MalformedObjectNameException {
         System.out.println("ManagementHandler started!");
         JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/karaf-root");
-        JMXConnector connector = JMXConnectorFactory.connect(url, null);
+        Map<String, String[]> environment = new HashMap<>();
+        String[] creds = {"karaf", "karaf"};
+        environment.put(JMXConnector.CREDENTIALS, creds);
+        JMXConnector connector = JMXConnectorFactory.connect(url, environment);
         MBeanServerConnection mbeanServer = connector.getMBeanServerConnection();
-        ObjectName systemMBean = new ObjectName("org.apache.karaf:type=config,name=*");
+        ObjectName systemMBean = new ObjectName("org.apache.karaf:type=config,name=root");
         ConfigMBean bean = (ConfigMBean) JMX.newMBeanProxy(mbeanServer, systemMBean, ConfigMBean.class);
         bean.setProperty("eu.netide.core.shimconnectivity", "port", "12345");
         System.out.println("ManagementHandler set port to 12345!");
