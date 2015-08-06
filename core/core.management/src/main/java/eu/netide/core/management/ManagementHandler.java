5ffa8e27a191cdd0942910dda9721e3e7ff141a8
Tim Vinkemeier <timvinkemeier@live.de>
2015-08-06 17:14:20 +0200

    Added first version of management interface

---
+++ core/core.management/src/main/java/eu/netide/core/management/ManagementHandler.java
@@ -0,0 +1,31 @@
+package eu.netide.core.management;
+
+import org.apache.karaf.config.core.ConfigMBean;
+
+import javax.management.*;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
+
+/**
+ * Created by timvi on 06.08.2015.
+ */
+public class ManagementHandler {
+
+    public void Start() throws IOException, MBeanException, MalformedObjectNameException {
+        System.out.println("ManagementHandler started!");
+        JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/karaf-root");
+        JMXConnector connector = JMXConnectorFactory.connect(url, null);
+        MBeanServerConnection mbeanServer = connector.getMBeanServerConnection();
+        ObjectName systemMBean = new ObjectName("org.apache.karaf:type=config,name=*");
+        ConfigMBean bean = (ConfigMBean) JMX.newMBeanProxy(mbeanServer, systemMBean, ConfigMBean.class);
+        bean.setProperty("eu.netide.core.shimconnectivity", "port", "12345");
+        System.out.println("ManagementHandler set port to 12345!");
+        connector.close();
+    }
+
+    public void Stop() {
+
+    }
+}
---
+++ core/core.management/src/main/java/eu/netide/core/management/ZeroMQBasedManagementConnector.java
@@ -0,0 +1,7 @@
+package eu.netide.core.management;
+
+/**
+ * Created by timvi on 06.08.2015.
+ */
+public class ZeroMQBasedManagementConnector implements IManagementConnector {
+}
