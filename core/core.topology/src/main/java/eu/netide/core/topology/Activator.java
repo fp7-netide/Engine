36707c6efff1e693a7e7aad673afea750d196772
Arne Schwabe <arne@rfc2549.org>
2015-09-09 13:15:38 +0200

    Fix bugs in the topology module. Add parent definitions to pom files

---
+++ core/core.topology/src/main/java/eu/netide/core/topology/Activator.java
@@ -0,0 +1,19 @@
+package eu.netide.core.topology;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created by arne on 07.09.15.
+ */
+public class Activator implements BundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
