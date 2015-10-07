51f48eed90e341e7ca60b4ddc4d1446c3a2a0a8e
Gregor Best <gbe@mail.upb.de>
2015-10-07 13:31:14 +0200

    Loader: install ansible from tarballs
    Signed-off-by: Gregor Best <gbe@mail.upb.de>

--- loader/setup.sh
+++ loader/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install --yes python-pip python3-pip git libzmq-dev ansible
+sudo apt-get install --yes python-pip python3-pip git libzmq-dev
 sudo pip3 install -r requirements.txt
 
-cd ~
-git clone git://github.com/ansible/ansible.git --recursive
-cd ansible
+# URL=https://releases.ansible.com/ansible/ansible-latest.tar.gz
+# At least ansible 2.0.0 is required
+URL=https://releases.ansible.com/ansible/ansible-2.0.0-0.3.beta1.tar.gz
+
+a=`mktemp -d`
+cd "$a"
+wget -O - "$URL" | gzip -dc | tar x
+cd ansible-*
 sudo python setup.py install
+cd
+sudo rm -r "$a"
