1513ed6f8fee8281ec6d998b64f8754c0999b60c
Arne Schwabe <arne@rfc2549.org>
2015-10-23 14:14:02 +0200

    Play around with onos

---
+++ core/core.onossb/src/main/java/Driver.java
@@ -0,0 +1,7 @@
+/**
+ * Created by arne on 22.10.15.
+ */
+public class Driver {
+
+
+}
---
+++ core/core.onossb/src/main/java/OnosSBManager.java
@@ -0,0 +1,15 @@
+import eu.netide.core.api.IShimMessageListener;
+import eu.netide.lib.netip.Message;
+
+/**
+ * Created by arne on 22.10.15.
+ */
+public class OnosSBManager implements IShimMessageListener {
+
+
+    @Override
+    public void OnShimMessage(Message message, String originId) {
+
+
+    }
+}
