1aaa9111791db1ef35cc0efcd0c094562b54eb60
Kévin Phemius <kevin.phemius@live.fr>
2016-01-05 15:55:49 +0100

    Changed name

---
+++ floodlight-backend/v1.2/main/java/net/floodlightcontroller/interceptor/ICoreListener.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 NetIDE Consortium and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package net.floodlightcontroller.interceptor;
+
+import eu.netide.lib.netip.Protocol;
+import eu.netide.lib.netip.ProtocolVersions;
+import java.util.List;
+import org.javatuples.Pair;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+
+/**
+ * @author giuseppex.petralia@intel.com
+ *
+ */
+public interface CoreListener {
+    void onOpenFlowCoreMessage(Long datapathId, OFMessage msg, int moduleId);
+
+    void onHelloCoreMessage(List<Pair<Protocol, ProtocolVersions>> requiredVersion, int moduleId);
+
+}
