ab40cd76043237972dca9d3c19d9ba6610b47fc2
peppepetra <giuseppex.petralia@intel.com>
2016-01-08 15:06:14 +0000

    Replace org.opendaylight.netide with eu.netide.util in groupdId and namespaces

--- odl/topology-update/impl/src/main/java/eu/netide/util/topology/update/impl/NotificationProducer.java
+++ odl/topology-update/impl/src/main/java/eu/netide/util/topology/update/impl/NotificationProducer.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netide.topology.update.impl;
+package eu.netide.util.topology.update.impl;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
--- odl/topology-update/impl/src/main/java/eu/netide/util/topology/update/impl/TopologyUpdateConsumer.java
+++ odl/topology-update/impl/src/main/java/eu/netide/util/topology/update/impl/TopologyUpdateConsumer.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netide.topology.update.impl;
+package eu.netide.util.topology.update.impl;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ConsumerContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareConsumer;
