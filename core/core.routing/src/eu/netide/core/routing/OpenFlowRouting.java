2b7953ef60db03d5ce7528f7c974fb88541f3fee
Arne Schwabe <arne@rfc2549.org>
2016-06-29 10:46:46 +0200

    Skeleton of core.routing and fix felix complaining

--- core/core.routing/src/eu/netide/core/routing/OpenFlowRouting.java
+++ core/core.routing/src/eu/netide/core/routing/OpenFlowRouting.java
@@ -7,20 +7,28 @@ import eu.netide.core.api.IShimManager;
 import eu.netide.core.api.IShimMessageListener;
 import eu.netide.core.api.MessageHandlingResult;
 import eu.netide.lib.netip.Message;
+import eu.netide.lib.netip.MessageType;
+import eu.netide.lib.netip.OpenFlowMessage;
 import org.apache.felix.scr.annotations.Component;
 
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.apache.felix.scr.annotations.Service;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFStatsReply;
+import org.projectfloodlight.openflow.protocol.OFStatsRequest;
 
 import java.util.LinkedList;
 
 /**
  * Created by arne on 28.06.16.
  */
-@Component(immediate=true)
+@Component(immediate = true)
 @Service
 public class OpenFlowRouting implements IAsyncRequestManager, IShimMessageListener, IBackendMessageListener {
+    private static Class OFAsyncRequests[] = {OFStatsRequest.class};
+    private static Class OFAsyncResults[] = {OFStatsReply.class};
+
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     private IBackendManager backendManager;
 
@@ -34,19 +42,58 @@ public class OpenFlowRouting implements IAsyncRequestManager, IShimMessageListen
 
     @Override
     public MessageHandlingResult OnShimMessage(Message message, String originId) {
+        if (message.getHeader().getMessageType() != MessageType.OPENFLOW) {
+            return MessageHandlingResult.RESULT_PASS;
+        }
+        if (message instanceof OpenFlowMessage) {
+            OFMessage openFlowMessage = ((OpenFlowMessage) message).getOfMessage();
 
+            for (Class respClass : OFAsyncResults) {
+                if (respClass.isInstance(openFlowMessage)) {
+                    handleAsyncResponse(openFlowMessage, originId);
+                    return MessageHandlingResult.RESULT_PROCESSED;
+                }
+
+            }
+        }
         return MessageHandlingResult.RESULT_PASS;
     }
+
+
+
+    @Override
+    public MessageHandlingResult OnBackendMessage(Message message, String
+            originId) {
+
+        if (message.getHeader().getMessageType() != MessageType.OPENFLOW) {
+            return MessageHandlingResult.RESULT_PASS;
+        }
+
+        if (message instanceof OpenFlowMessage) {
+            OFMessage openFlowMessage = ((OpenFlowMessage) message).getOfMessage();
+
+            for (Class reqClass : OFAsyncRequests)
+                if (reqClass.isInstance(openFlowMessage)) {
+                    handleAsyncRequest(openFlowMessage, originId);
+                    return MessageHandlingResult.RESULT_PROCESSED;
+                }
+
+        }
+        return MessageHandlingResult.RESULT_PASS;
+    }
+
+
+    private void handleAsyncRequest(OFMessage openFlowMessage, String originId) {
+
+    }
+    private void handleAsyncResponse(OFMessage openFlowMessage, String originId) {
+
+    }
+
 
     @Override
     public void OnOutgoingShimMessage(Message message) {
 
-    }
-
-    @Override
-    public MessageHandlingResult OnBackendMessage(Message message, String originId) {
-
-        return MessageHandlingResult.RESULT_PASS;
     }
 
     @Override
